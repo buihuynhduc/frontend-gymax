de18d62577ca44a33ae876f0e9da48f232547a5d
buihuynhduc <huynhduc01693495928@gmail.com>
2021-11-25 13:54:59 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-$(document).ready(function () {
-    $('.nav-icon').click(function (e) { 
-        e.preventDefault();
-        $('.introduce-inner').hide();;
-        $('.cover').fadeIn(2000);
-        $('.menu').fadeIn(1000);
-    });
-    $('.cover').click(function (e) { 
-        e.preventDefault();
-        $('.menu').fadeOut(2000);
-        $('.cover').fadeOut(2000);
-        $('.introduce-inner').fadeIn(2000);
-    });
-   //tab
-});
+// 
 
 
